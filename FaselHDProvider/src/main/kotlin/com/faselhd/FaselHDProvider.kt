362da32f43af6b59fa745c94b45eb750d509daae
fyritp <halimtestmath@gmail.com>
2024-05-08 14:17:12 +0100

    Update FaselHDProvider.kt

--- FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
+++ FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
@@ -12,8 +12,8 @@ import org.jsoup.nodes.Element
 
 class FaselHD : MainAPI() {
     override var lang = "ar"
-    override var mainUrl = "https://faselhd.express"
-    private  val alternativeUrl = "https://www.faselhd.club"
+    override var mainUrl = "https://www.faselhds.world"
+    private  val alternativeUrl = "https://faselhd.center"
     override var name = "FaselHD"
     override val usesWebView = false
     override val hasMainPage = true
